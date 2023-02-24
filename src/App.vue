afbe622be00c7b4772a87108d89a23ae0bad1dd0
LinBii <aa68968@gmail.com>
2023-02-24 09:22:30 +0800

    add basic HTML structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,121 @@
 <script setup></script>
 
 <template>
-  <section>This is a test website</section>
+  <section>
+    <h1>There is no one</h1>
+    <h1>who loves pain</h1>
+    <button>F O R M</button>
+    <div>
+      <div>
+        <div>
+          <h2>paragraph</h2>
+        </div>
+        <ol>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+        </ol>
+      </div>
+      <div>
+        <div>
+          <h2>paragraph</h2>
+        </div>
+        <ol>
+          <li>
+            Quisque sodales leo <strong><u>vitae vulputate auctor.</u></strong>
+          </li>
+          <li>
+            Proin ac justo ut nisl tincidunt imperdiet.Maecenas viverra libero a
+            pellentesque blandit.
+          </li>
+          <li>
+            Cras tristique tellus id leo bibendum, eu dapibus nisl accumsan.
+          </li>
+          <li>
+            Donec ultrices sapien
+            <strong>vitae leo venenatis ullamcorper.</strong>
+          </li>
+        </ol>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="wave">Wave</div>
+  </section>
+  <section>
+    <h1>F O R M</h1>
+    <form action="">
+      <div>
+        <label for="store">store *</label>
+        <input
+          type="text"
+          name="store"
+          id="store"
+          placeholder="placeholder text"
+          required
+        />
+      </div>
+      <div>
+        <label for="name">name *</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="placeholder text"
+          required
+        />
+      </div>
+      <div>
+        <label for="phone">phone *</label>
+        <input
+          type="number"
+          name="phone"
+          id="phone"
+          placeholder="placeholder text"
+          required
+        />
+      </div>
+      <div>
+        <label for="consumption">Amount of consumption *</label>
+        <input
+          type="number"
+          name="consumption"
+          id="consumption"
+          placeholder="placeholder text"
+          required
+        />
+      </div>
+      <div>
+        <label for="payment">payment *</label>
+        <select name="payment" id="payment" required>
+          <option value="digital payment">digital payment</option>
+          <option value="atm">ATM</option>
+        </select>
+      </div>
+      <button>submit</button>
+    </form>
+  </section>
+  <section>
+    <div class="cards">
+      <div class="card">A</div>
+      <div class="card">B</div>
+      <div class="card">C</div>
+      <div class="card">D</div>
+      <div class="card">E</div>
+      <div class="card">F</div>
+      <div class="card">G</div>
+      <div class="card">H</div>
+    </div>
+  </section>
+  <section>
+    <h1>13.32</h1>
+    <h1>who seeks after it</h1>
+    <h1>and wants to have it</h1>
+    <div>lighthouse</div>
+  </section>
+  <section>
+    <h1>Neque porro quisquam</h1>
+    <h1>est qui dolorem!</h1>
+    <div>market picture</div>
+  </section>
 </template>
 
 <style scoped></style>
