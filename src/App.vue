685eec904cc15e406db1940c5926acda075ef219
LinBii <aa68968@gmail.com>
2023-02-27 20:33:52 +0800

    add skeleton css

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <script setup></script>
 
 <template>
-  <section>
-    <h1>There is no one</h1>
-    <h1>who loves pain</h1>
-    <button>F O R M</button>
-    <div>
+  <section class="background">
+    <div class="topic">
+      <h1 class="topic first">There is no one</h1>
+      <h1 class="topic second">who loves pain</h1>
+      <a role="button" href="#form" class="button">FORM</a>
+    </div>
+    <div class="paragraph-block">
       <div>
-        <div>
-          <h2>paragraph</h2>
+        <div class="container">
+          <img src="../src/assets/icon/cactus.svg" />
+          <h2 class="paragraph">paragraph</h2>
+          <img src="../src/assets/icon/cactus.svg" />
         </div>
-        <ol>
+        <ol class="text">
           <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
         </ol>
       </div>
       <div>
-        <div>
-          <h2>paragraph</h2>
+        <div class="container">
+          <img src="../src/assets/icon/cactus.svg" />
+          <h2 class="paragraph">paragraph</h2>
+          <img src="../src/assets/icon/cactus.svg" />
         </div>
-        <ol>
+        <ol class="text">
           <li>
             Quisque sodales leo <strong><u>vitae vulputate auctor.</u></strong>
           </li>
@@ -40,60 +46,70 @@
   <section>
     <div class="wave">Wave</div>
   </section>
-  <section>
-    <h1>F O R M</h1>
+  <section class="content">
+    <h1 id="form">FORM</h1>
     <form action="">
-      <div>
-        <label for="store">store *</label>
-        <input
-          type="text"
-          name="store"
-          id="store"
-          placeholder="placeholder text"
-          required
-        />
+      <div class="form-list">
+        <div>
+          <label for="store">store </label>
+          <div class="asterisk">*</div>
+          <input
+            type="text"
+            name="store"
+            id="store"
+            class="form-input"
+            placeholder="placeholder text"
+            required
+          />
+        </div>
+        <div>
+          <label for="name">name </label>
+          <div class="asterisk">*</div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            class="form-input"
+            placeholder="placeholder text"
+            required
+          />
+        </div>
+        <div>
+          <label for="phone">phone </label>
+          <div class="asterisk">*</div>
+          <input
+            type="number"
+            name="phone"
+            id="phone"
+            class="form-input"
+            placeholder="placeholder text"
+            required
+          />
+        </div>
+        <div>
+          <label for="consumption">Amount of consumption </label>
+          <div class="asterisk">*</div>
+          <input
+            type="number"
+            name="consumption"
+            id="consumption"
+            class="form-input"
+            placeholder="placeholder text"
+            required
+          />
+        </div>
+        <div>
+          <label for="payment">payment </label>
+          <div class="asterisk">*</div>
+          <select name="payment" id="payment" class="form-input" required>
+            <option value="digital payment">digital payment</option>
+            <option value="atm">ATM</option>
+          </select>
+        </div>
       </div>
-      <div>
-        <label for="name">name *</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="placeholder text"
-          required
-        />
-      </div>
-      <div>
-        <label for="phone">phone *</label>
-        <input
-          type="number"
-          name="phone"
-          id="phone"
-          placeholder="placeholder text"
-          required
-        />
-      </div>
-      <div>
-        <label for="consumption">Amount of consumption *</label>
-        <input
-          type="number"
-          name="consumption"
-          id="consumption"
-          placeholder="placeholder text"
-          required
-        />
-      </div>
-      <div>
-        <label for="payment">payment *</label>
-        <select name="payment" id="payment" required>
-          <option value="digital payment">digital payment</option>
-          <option value="atm">ATM</option>
-        </select>
-      </div>
-      <button>submit</button>
+
+      <button class="button submit">submit</button>
     </form>
-  </section>
-  <section>
     <div class="cards">
       <div class="card">A</div>
       <div class="card">B</div>
@@ -104,17 +120,21 @@
       <div class="card">G</div>
       <div class="card">H</div>
     </div>
-  </section>
-  <section>
-    <h1>13.32</h1>
-    <h1>who seeks after it</h1>
-    <h1>and wants to have it</h1>
-    <div>lighthouse</div>
-  </section>
-  <section>
-    <h1>Neque porro quisquam</h1>
-    <h1>est qui dolorem!</h1>
-    <div>market picture</div>
+    <div class="content motto">
+      <h1 class="content motto number">13.32</h1>
+      <h1>who seeks after it and</h1>
+      <h1>wants to have it</h1>
+      <img src="../src/assets/icon/lighthouse.svg" class="lighthouse" />
+    </div>
+    <div class="shop">
+      <div class="topic">
+        <h1 class="bottom">
+          Neque porro quisquam <br />
+          est qui dolorem!
+        </h1>
+        <img src="../src/assets/icon/market.svg" class="market" />
+      </div>
+    </div>
   </section>
 </template>
 
